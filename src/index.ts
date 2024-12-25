400cbe3977153c58ed731c7d5ce3ed386e8e66df
karananeja <karananeja.r@gmail.com>
2024-12-25 13:05:38 +0530

    chore: add comments for better readability

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
+// Importing the required dependencies into the application
 import cors from 'cors';
 import { config } from 'dotenv';
 import express, { Request, Response } from 'express';
 
 config();
 
+// Initializing the application
 const app = express();
-const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Setting up the port
+const port = process.env.PORT || 3000;
+
+// Restrict all miscellaneous routes
 app.get('*', (_: Request, res: Response) => {
   res.status(404).send('Not found');
 });
 
+// Server started on the required port
 app.listen(port, () => {
   console.log(`[server] The port is listening on ${port}`);
 });
