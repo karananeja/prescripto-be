be615d20c8e1af782ec322359a148c56647db253
karananeja <karananeja.r@gmail.com>
2024-12-26 19:05:58 +0530

    feat: create the user and doctor model

---
+++ src/models/doctorModel.ts
@@ -0,0 +1,22 @@
+import { model, models, Schema } from 'mongoose';
+
+const doctorSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    image: { type: String, required: true },
+    specialty: { type: String, required: true },
+    degree: { type: String, required: true },
+    experience: { type: String, required: true },
+    about: { type: String, required: true },
+    available: { type: Boolean, required: true },
+    fee: { type: Number, required: true },
+    address: { type: Object, required: true },
+    date: { type: Number, required: true },
+    slotsBooked: { type: Object, default: {} },
+  },
+  { minimize: false }
+);
+
+export const doctorModel = models.doctor || model('doctor', doctorSchema);
---
+++ src/models/userModel.ts
@@ -0,0 +1,16 @@
+import { model, models, Schema } from 'mongoose';
+
+import { defaultImageInBase64 } from '../utils/helpers';
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  image: { type: String, default: defaultImageInBase64 },
+  address: { type: Object, default: { line1: '', line2: '' } },
+  gender: { type: String, default: 'Not Selected' },
+  dob: { type: String, default: 'Not Selected' },
+  phone: { type: String, default: '0000000000' },
+});
+
+export const userModel = models.user || model('user', userSchema);
